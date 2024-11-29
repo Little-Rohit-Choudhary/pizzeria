14686b033130ffa73e71be9945d0022181158b3a
Little-Rohit-Choudhary <choudharyrohit1401@gmail.com>
2024-11-29 08:51:49 +0530

    Updated index.html, OrderForm.jsx, and vite.config.js: - Modified index.html for layout improvements - Updated OrderForm.jsx to include enhanced functionality - Adjusted vite.config.js for better build configuration

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/pizzeria/"
+  base: "/pizzeria/",
+  server: {
+    host: true,  // This makes Vite listen on all available network interfaces
+    port: 3000,  // You can specify any port here
+  },
 })
